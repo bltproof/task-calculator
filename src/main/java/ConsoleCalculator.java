d4b1c5edf7b22342fda06c99524730418c888f6c
ShaipovZA <z.shaipov@id-mt.ru>
2020-07-15 19:01:02 +0300

    task-calculator fixes

--- src/main/java/ConsoleCalculator.java
+++ src/main/java/ConsoleCalculator.java
@@ -1,14 +1,20 @@
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class ConsoleCalculator {
+    private String expression;
 
-    public static String ExpressionToRPN(String expression) {
+    public ConsoleCalculator(String expression) {
+        this.expression = expression;
+    }
+
+    private static String ExpressionToRPN(String expression) {
         StringBuilder result = new StringBuilder();
         Stack<Character> stack = new Stack<>();
 
         for (int i = 0; i < expression.length(); i++) {
             char token = expression.charAt(i);
+
             int priority = getPriority(token);
 
             if (priority == 0) result.append(token);
@@ -18,8 +24,10 @@ public class ConsoleCalculator {
                 result.append(' ');
 
                 while (!stack.empty()) {
-                    if (getPriority(stack.peek()) >= priority) result.append(stack.pop());
-                    else break;
+                    if (getPriority(stack.peek()) >= priority) {
+                        result.append(stack.pop());
+                        result.append(' ');
+                    } else break;
                 }
                 stack.push(token);
             }
@@ -36,11 +44,19 @@ public class ConsoleCalculator {
         while (!stack.empty()) {
             result.append(stack.pop());
         }
+        //(?!10$)([0-9]{2})
+        //[2-9[1-9]]{2,}
+        String str = "2*3+(44 / 2)";
+        Pattern pattern = Pattern.compile("[2-9[1-9]]{2,}");
+
+        if (pattern.matcher(str).matches()) {
+            throw new RuntimeException("Ошибка! Цифры только от 1 до 10!");
+        }
 
         return result.toString();
     }
 
-    public static int RPNToAnswer(String rpn) {
+    private static int RPNToAnswer(String rpn) {
         StringBuilder sb = new StringBuilder();
         Stack<Integer> stack = new Stack<>();
 
@@ -60,10 +76,15 @@ public class ConsoleCalculator {
             if (getPriority(rpn.charAt(i)) > 1) {
                 int a = stack.pop(), b = stack.pop();
 
-                if (rpn.charAt(i) == '+') stack.push(b + a);
-                if (rpn.charAt(i) == '-') stack.push(b - a);
-                if (rpn.charAt(i) == '*') stack.push(b * a);
-                if (rpn.charAt(i) == '/') stack.push(b / a);
+                try {
+                    if (rpn.charAt(i) == '+') stack.push(b + a);
+                    if (rpn.charAt(i) == '-') stack.push(b - a);
+                    if (rpn.charAt(i) == '*') stack.push(b * a);
+                    if (rpn.charAt(i) == '/') stack.push(b / a);
+
+                } catch (ArithmeticException e) {
+                    System.out.println("Ошибка! Деление на ноль.");
+                }
             }
         }
 
@@ -78,11 +99,29 @@ public class ConsoleCalculator {
         return 0;
     }
 
+    private static String convert(Scanner scanner) {
+
+    }
+
     public static void main(String[] args) {
 
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println(RPNToAnswer(ExpressionToRPN(scanner.nextLine())));
-        }
+        System.out.println("Калькулятор умеет работать с римскими или арабскими цифрами");
+        System.out.println("Введите выражение одной строкой");
 
+        try (Scanner scanner = new Scanner(System.in)) {
+            String str = scanner.nextLine();
+            String[] array = str.split("[+\\-*/()]");
+
+            for (int i = 0; i < array.length; i++) {
+                String romanLetter = array[i].trim();
+                if (!romanLetter.isEmpty()) array[i] = String.valueOf(RomanNumeral.romanToArabic(romanLetter));
+                str = str.replace(romanLetter, array[i]);
+            }
+
+            System.out.println(str);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
