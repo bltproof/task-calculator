f5d56e61d5393fcdd3f6541754d741531903639b
ShaipovZA <z.shaipov@id-mt.ru>
2020-07-16 15:03:28 +0300

    task-calculator fixes

--- src/main/java/CalculatorApp.java
+++ src/main/java/CalculatorApp.java
@@ -5,22 +5,22 @@ public class CalculatorApp {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
 
-            System.out.print("Введите выражение одной строкой: ");
+            System.out.println("Введите выражение одной строкой: ");
 
             String expression = scanner.nextLine();
 
-            ReversePolishNotation rpnClass = new ReversePolishNotation();
+            ReversePolishNotation notation = new ReversePolishNotation();
             ArabicRomanConversion conversion = new ArabicRomanConversion();
 
             if (Character.isDigit(expression.toCharArray()[0]) || Character.isDigit(expression.toCharArray()[1])) {
-                String rpnString = rpnClass.expressionToRpn(expression);
-                int result = rpnClass.rpnToResult(rpnString);
+                String rpnString = notation.expressionToRpn(expression);
+                int result = notation.rpnToResult(rpnString);
                 System.out.println(result);
 
             } else {
                 String preparedExpression = conversion.prepare(expression);
-                String rpnString = rpnClass.expressionToRpn(preparedExpression);
-                int result = rpnClass.rpnToResult(rpnString);
+                String rpnString = notation.expressionToRpn(preparedExpression);
+                int result = notation.rpnToResult(rpnString);
                 String romanResult = conversion.arabicToRoman(result);
                 System.out.println(romanResult);
             }
